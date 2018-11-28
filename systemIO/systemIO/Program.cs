1b169fbc6a4f62e22dac9004b83b47ed12b62dca
Roko Maršić <45421234+plaut1337@users.noreply.github.com>
2018-11-28 12:23:55 +0100

    Add files via upload

---
+++ systemIO/systemIO/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace systemIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string putanja = @"C:\Users\Korisnik\Documents";
+            if(Directory.Exists(putanja))
+            {
+                Console.WriteLine("Datoteke: ");
+                foreach (string datoteka in Directory.GetFiles(putanja))
+                {
+                    Console.WriteLine(datoteka);
+                }
+            }
+            string mapa1 = putanja + @"\test";
+            if (Directory.Exists(mapa1))
+            {
+                Console.WriteLine("\nMapa izbrisana.");
+                Directory.Delete(mapa1);
+            }
+            else
+            {
+                Console.WriteLine("\nMapa kreirana.");
+                Directory.CreateDirectory(mapa1);
+            }
+           if (Directory.Exists(putanja))
+            {
+                Console.WriteLine("\nPodmape:");
+                foreach (string datoteka in Directory.GetDirectories(putanja))
+                {
+                    Console.WriteLine(datoteka);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
