6e0d43d8b1c916fd8fc34e5400fa8e9055012a71
Roko Maršić <45421234+plaut1337@users.noreply.github.com>
2019-01-16 17:56:46 +0100

    Add files via upload

---
+++ systemIO/MarsicRokoUsing/MarsicRokoUsing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsicRokoUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+            Console.WriteLine("\n-- Zapisujemo u datoteku...");
+            using (StreamWriter sw = new StreamWriter(@"C:\BOOP\datoteka.txt"))
+            {
+                sw.WriteLine("Ime: {0}", ime);
+                sw.WriteLine("Prezime: {0}", prezime);
+                Console.WriteLine("\n-- Pročitano iz datoteke:");
+            }
+                using (StreamReader sr = new StreamReader(@"C:\BOOP\datoteka.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+                }
+                Console.ReadKey();
+        }
+    }
+}
