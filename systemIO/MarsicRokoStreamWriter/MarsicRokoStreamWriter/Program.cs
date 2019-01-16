926323dc4aa84a89921a921ff48e8b884bbce08c
Roko Maršić <45421234+plaut1337@users.noreply.github.com>
2019-01-16 17:32:16 +0100

    Add files via upload

---
+++ systemIO/MarsicRokoStreamWriter/MarsicRokoStreamWriter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsicRokoStreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\NOOP\datoteka.txt");
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
